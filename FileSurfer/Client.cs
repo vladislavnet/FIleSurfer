22e396795815421e4c389072786b662ad47f57c9
vladislavnet <vladislaus.panyukov@yandex.ru>
2020-10-07 19:55:36 +0300

    Edit Client.cs

--- FileSurfer/Client.cs
+++ FileSurfer/Client.cs
@@ -8,5 +8,31 @@ namespace FileSurfer
 {
     internal class Client
     {
+        private string uri;
+        private string login;
+        private string password;
+        private int bufferSize = 1024;
+
+        public Client(string uri, string login, string password)
+        {
+            this.uri = uri;
+            this.login = login;
+            this.password = password;
+        }
+
+        public bool IsPassive { get; set; } = true;
+        public bool IsBinary { get; set; } = true;
+        public bool IsEnableSSL { get; set; } = false;
+        public bool IsHash { get; set; } = false;
+
+        public string[] ListDirectory()
+        {
+            return null;
+        }
+
+        public string[] ListDirectoryDetails()
+        {
+            return null;
+        }
     }
 }
