eadf8e8ef0a73746d7257cd1c5bfcab904e120f5
vladislavnet <vladislaus.panyukov@yandex.ru>
2020-10-08 18:39:36 +0300

    Filled in class "FileDirectoryInfo"

--- FileSurfer/FileDirectoryInfo.cs
+++ FileSurfer/FileDirectoryInfo.cs
@@ -8,6 +8,20 @@ namespace FileSurfer
 {
     public class FileDirectoryInfo
     {
+        public FileDirectoryInfo() { }
+        public FileDirectoryInfo(string name, string type, string adress, string fileSize,   string date )
+        {
+            Size = fileSize;
+            Type = type;
+            Name = name;
+            Date = date;
+            Address = adress;
+        }
 
+        public string Size { get; set; }
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public string Date { get; set; }
+        public string Address { get; set; }
     }
 }
